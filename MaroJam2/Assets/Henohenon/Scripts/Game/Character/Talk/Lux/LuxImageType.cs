6ce25a5e63f8d93cffc50d7f2c171d0c35fe430b
henohenon <takeru.hawk.son@gmail.com>
2026-02-20 22:18:08 +0900

    add types

--- MaroJam2/Assets/Henohenon/Scripts/Game/Character/Talk/Lux/LuxImageType.cs
+++ MaroJam2/Assets/Henohenon/Scripts/Game/Character/Talk/Lux/LuxImageType.cs
@@ -3,4 +3,12 @@
     // noneは既存のまま変えない、DefaultはDefaultに変更(どちらかというとSerializedDict用
     None,
     Default,
+    R_DOWN_EXCITING,
+    R_DOWN_KOMARU,
+    R_DOWN_NOMAL_CLOSE,
+    R_DOWN_NOMAL_OPEN,
+    R_DOWN_POKE,
+    R_DOWN_SHY,
+    R_DOWN_SMILE_CLOSE,
+    R_DOWN_SMILE_OPEN,
 }
