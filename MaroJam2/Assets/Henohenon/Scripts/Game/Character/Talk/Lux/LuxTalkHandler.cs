fe8eec4d2f554494980175e67353e5804251d2b3
henohenon <takeru.hawk.son@gmail.com>
2026-02-21 08:08:23 +0900

    Squashed commit of the following:
    commit fd362c8f64a95d2ed8668b911b89239a38ba61bb
    Author: henohenon <takeru.hawk.son@gmail.com>
    Date:   Sat Feb 21 08:07:20 2026 +0900
    
        add mini logics

--- MaroJam2/Assets/Henohenon/Scripts/Game/Character/Talk/Lux/LuxTalkHandler.cs
+++ MaroJam2/Assets/Henohenon/Scripts/Game/Character/Talk/Lux/LuxTalkHandler.cs
@@ -30,19 +30,26 @@ public class LuxTalkHandler : TalkHandler<LuxTalkType>, IDisposable
         switch (type)
         {
             case LuxTalkType.Tutorial:
-                await Text("こんにちは！\n合えて嬉しいです！");
-                await Text("初めまして！俺はルクス！\nここのことをあなたに教えてあげる！");
-                var answer = await Question("いらない", "わかった");
-                switch (answer)
+                MiniImage(LuxImageType.Mini_Holo);
+                try
                 {
-                    case SelectionType.Alpha:
-                        await Text("え？もう知ってるんですか？\nわかりました！\n俺をひいてくれるのを待ってるね！");
-                        break;
-                    case SelectionType.Beta:
-                        await Talk(LuxTalkType.Tutorial_Stay, token);
-                        break;
+                    await Text("こんにちは！\n合えて嬉しいです！");
+                    await Text("初めまして！俺はルクス！\nここのことをあなたに教えてあげる！");
+                    var answer = await Question("いらない", "わかった");
+                    switch (answer)
+                    {
+                        case SelectionType.Alpha:
+                            await Text("え？もう知ってるんですか？\nわかりました！\n俺をひいてくれるのを待ってるね！");
+                            break;
+                        case SelectionType.Beta:
+                            await Talk(LuxTalkType.Tutorial_Stay, token);
+                            break;
+                    }
                 }
-
+                finally
+                {
+                    Image(LuxImageType.Default);
+                }
                 break;
             case LuxTalkType.Tutorial_Stay:
                 await Text("えへへ、お話聞いてくれるんですね\n何から説明しようかな～");
@@ -58,6 +65,7 @@ public class LuxTalkHandler : TalkHandler<LuxTalkType>, IDisposable
                 await Text("仮想体からいえることは以上です！\n本物の俺と仲良くなって\nハッピーエンド目指しましょうね～");
                 break;
             case LuxTalkType.TutorialAgain:
+                MiniImage(LuxImageType.Mini_Holo);
                 await Text("は～い！わかりました！\nもう一回説明しますね");
                 await Talk(LuxTalkType.Tutorial, token);
                 break;
@@ -91,14 +99,25 @@ public class LuxTalkHandler : TalkHandler<LuxTalkType>, IDisposable
             await _onNext.FirstAsync(token);
         }
 
-        void Image(LuxImageType type)
+        void Image(LuxImageType t)
         {
-            if (_data.Images.TryGetValue(type, out var image)) _talkController.CharacterImage.sprite = image;
+            _talkController.MiniTalkButton.gameObject.SetActive(false);
+            _talkController.CharaTalkButton.gameObject.SetActive(true);
+            if (t == LuxImageType.None) return;
+            if (_data.Images.TryGetValue(t, out var image)) _talkController.CharacterImage.sprite = image;
         }
 
-        void Voice(LuxVoiceType type)
+        void MiniImage(LuxImageType t)
         {
-            if (_data.Voices.TryGetValue(type, out var voice)) _voicePlayer.Play(voice);
+            _talkController.MiniTalkButton.gameObject.SetActive(true);
+            _talkController.CharaTalkButton.gameObject.SetActive(false);
+            if (t == LuxImageType.None) return;
+            if (_data.Images.TryGetValue(t, out var image)) _talkController.MiniCharaImage.sprite = image;
+        }
+
+        void Voice(LuxVoiceType t)
+        {
+            if (_data.Voices.TryGetValue(t, out var voice)) _voicePlayer.Play(voice);
         }
 
         async UniTask<SelectionType> Question(string alpha, string beta)
