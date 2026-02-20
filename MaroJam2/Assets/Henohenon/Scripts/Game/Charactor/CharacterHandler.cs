2329f6085de2563b60865409dc509e467d49bb6c
henohenon <takeru.hawk.son@gmail.com>
2026-02-20 14:26:43 +0900

    add images & apply ui

---
+++ MaroJam2/Assets/Henohenon/Scripts/Game/Charactor/CharacterHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.TextCore.Text;
+
+public class CharacterHandler
+{
+    private readonly CharacterType _characterType;
+    private int _love;
+    private int _loveLv;
+    
+    public CharacterHandler(CharacterType type)
+    { 
+        _characterType = type;
+    }
+    
+    public CharacterType CharacterType => _characterType;
+    public int Love => _love;
+    public int LoveLv => _loveLv;
+    
+    public void AddLove(int value)
+    {
+        _love += value;
+        if (_love < 10)
+        {
+            _loveLv = 0;
+        }
+        else if (_love < 20)
+        {
+            _loveLv = 1;
+        }
+        else
+        {
+            _loveLv = 2;
+        }
+    }
+}
---
+++ MaroJam2/Assets/Henohenon/Scripts/LibUnity/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Henohenon.Scripts.LibUnity
+{
+    // https://naichilab.blogspot.com/2013/11/unitymanager.html
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+        static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = (T)FindObjectOfType(typeof(T));
+
+                    if(instance == null)
+                    {
+                        Debug.LogError( typeof(T) + " none");
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
